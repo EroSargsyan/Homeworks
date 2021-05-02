4e8ec307d13074513a5e8aa694e1671ea6f3b68e
Erik Sargsyan <erosargsyan7@gmail.com>
2021-05-02 20:46:17 +0400

    lesson_001&002_added

---
+++ lesson002/PrimitiveTypes.js
@@ -0,0 +1,28 @@
+/*let admin;
+let name;
+name="John";
+admin=name;
+console.log(admin)
+
+*/
+
+//i = 11;
+//n = 0;
+
+/*let a = 10;
+let b = 3;
+a = a * b;
+b = a / b;
+a = a / b;
+console.log(a);
+console.log(b);
+*/
+
+/*let a = 10;
+let b = 3;
+a = a - b;
+b = b + a;
+a = b - a;
+console.log(a);
+console.log(b);
+*/
