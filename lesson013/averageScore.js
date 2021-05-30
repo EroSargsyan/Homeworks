5a18e8183ec90d16607b768915ca7944d182c4d8
Erik Sargsyan <erosargsyan7@gmail.com>
2021-05-30 21:39:19 +0400

    fix: add improvements

--- lesson013/averageScore.js
+++ lesson013/averageScore.js
@@ -1,17 +1,29 @@
 let obj = {
   John: [100, 90, 80],
-  Bob: [100, 100, 90],
+  Bob: [100, 70, 80],
+  Menendes: [100, 100, 100],
+  Vaxinak: [100, 20, 10],
 };
-function getBestStudent(obj) {
-  let averageJohn =
-    obj.John.reduce((acc, item) => acc + item, 0) / obj.John.length;
-  let averageBob =
-    obj.Bob.reduce((acc, item) => acc + item, 0) / obj.Bob.length;
-  if (averageBob < averageJohn) {
-    return "John";
-  } else if (averageBob > averageJohn) {
-    return "Bob";
-  }
-}
 
+function getBestStudent(obj) {
+  let arr = Object.entries(obj);
+  let grade = 0;
+  let bestStud;
+  arr = arr.map(([key, value]) => {
+    return [
+      key,
+      value.reduce((acc, item) => {
+        return acc + item;
+      }, 0) / value.length,
+    ];
+  });
+
+  for (let [key, value] of arr) {
+    if (grade < value) {
+      grade = value;
+      bestStud = key;
+    }
+  }
+  return bestStud;
+}
 console.log(getBestStudent(obj));
