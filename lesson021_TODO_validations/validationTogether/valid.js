3990a1892180a12df34a9cdb58bc92ae98242f11
Erik Sargsyan <erosargsyan7@gmail.com>
2021-06-24 22:22:31 +0400

    fix: add validations via regex

--- lesson021_TODO_validations/validationTogether/valid.js
+++ lesson021_TODO_validations/validationTogether/valid.js
@@ -1,22 +1,10 @@
 //////NumberValidation////////////
 let text = document.querySelector(".textTel");
 let tel = document.querySelector("#tel");
-let arrNumber = [];
+let numPattern = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
 
-tel.addEventListener("input", (event) => {
-  if (event.data != null && (Number(event.data) || event.data === "+")) {
-    arrNumber.push(event.data);
-  } else if (event.data == null) {
-    arrNumber.pop();
-  }
-  console.log(arrNumber);
-  if (
-    arrNumber[0] === "+" &&
-    arrNumber[1] === "3" &&
-    arrNumber[2] === "7" &&
-    arrNumber[3] === "4" &&
-    arrNumber.length === 12
-  ) {
+tel.addEventListener("input", () => {
+  if (tel.value.match(numPattern)) {
     text.textContent = "Valid number";
   } else {
     text.textContent = "Invalid number";
@@ -24,21 +12,14 @@ tel.addEventListener("input", (event) => {
 });
 
 //////////////EmailValidation///////////
-let regexp =
+let emailPattern =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 let email = document.querySelector("#email");
 let textEmail = document.querySelector(".textEmail");
-let arrEmail = [];
-email.addEventListener("input", (event) => {
-  if (event.data != null) {
-    arrEmail.push(event.data);
-  } else if (event.data == null) {
-    arrEmail.pop();
-  }
-  console.log(arrEmail);
 
-  if (String(arrEmail.join("")).match(regexp)) {
+email.addEventListener("input", () => {
+  if (email.value.match(emailPattern)) {
     textEmail.textContent = "Valid Email";
   } else {
     textEmail.textContent = "Invalid Email";
