16e4f1f43aa2558257cc4cf202f37aa47d0287b5
Erik Sargsyan <erosargsyan7@gmail.com>
2021-06-29 20:24:12 +0400

    feat(books):add books fetch with pagination

---
+++ lesson022_fetchAPI/booksFetch/main.js
@@ -0,0 +1,115 @@
+const inputField = document.getElementById("inputField");
+const searchButton = document.getElementById("searchButton");
+const numFound = document.getElementById("numOfResults");
+const resultsDiv = document.getElementById("resultsContainer");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+let page = 1;
+
+searchButton.addEventListener("click", () => {
+  fetching(createURL(page));
+});
+
+function createURL(page) {
+  const srchWithPluses = inputField.value.trim().split(" ").join("+");
+  return `http://openlibrary.org/search.json?q=${srchWithPluses}&page=${page}`;
+}
+
+function fetching(url) {
+  fetch(url)
+    .then((result) => result.json())
+    .then((jsonData) => {
+      countOfSearches(jsonData);
+      allFunctions(jsonData);
+    });
+}
+
+inputField.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    searchButton.click();
+  }
+});
+
+nextBtn.addEventListener("click", () => {
+  if (page < 6 && page >= 1) {
+    page++;
+    resultsDiv.innerHTML = "";
+
+    fetching(createURL(page));
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  if (page <= 6 && page > 1) {
+    page--;
+    resultsDiv.innerHTML = "";
+    fetching(createURL(page));
+  }
+});
+
+function countOfSearches(param) {
+  const pNumFound = document.createElement("p");
+  pNumFound.setAttribute("id", "numOfResults");
+  pNumFound.textContent = `Total count of results: ${param.numFound}`;
+  resultsDiv.appendChild(pNumFound);
+  const br = document.createElement("br");
+  resultsDiv.appendChild(br);
+}
+
+function title(param, i) {
+  const pTitle = document.createElement("p");
+  pTitle.setAttribute("id", "title");
+  pTitle.textContent = `Title: ${param.docs[i].title}`;
+  resultsDiv.appendChild(pTitle);
+}
+
+function authorName(param, i) {
+  const pAuthor = document.createElement("p");
+  pAuthor.setAttribute("id", "author");
+  pAuthor.textContent = `Author name: ${param.docs[i].author_name}`;
+  resultsDiv.appendChild(pAuthor);
+}
+
+function publishYear(param, i) {
+  const pPublishyear = document.createElement("p");
+  pPublishyear.setAttribute("id", "publishYear");
+  pPublishyear.textContent = `First publish year: ${param.docs[i].first_publish_year}`;
+  resultsDiv.appendChild(pPublishyear);
+}
+
+function subjects(param, i) {
+  const pSubject = document.createElement("p");
+  pSubject.setAttribute("id", "subject");
+  if (param.docs[i].hasOwnProperty("subject")) {
+    for (let j = 0; j < 4; j++) {
+      if (
+        param.docs[i].subject[j] !== undefined &&
+        param.docs[i].subject[j] != "Protected DAISY"
+      ) {
+        if (j === 0) {
+          pSubject.textContent = `Subject: ${param.docs[i].subject[j]} `;
+        } else {
+          pSubject.textContent += `, ${param.docs[i].subject[j]}`;
+        }
+      }
+      resultsDiv.appendChild(pSubject);
+    }
+  }
+}
+
+/////////add results to different divs ////
+function allFunctions(param) {
+  // const allFuncDiv = document.createElement("div");
+  // allFuncDiv.setAttribute("id", "allFuncDiv");
+  // resultsDiv.append(allFuncDiv);
+
+  // let eachFunc = function (param) {
+  for (let i = 0; i < param.docs.length; i++) {
+    title(param, i);
+    authorName(param, i);
+    publishYear(param, i);
+    subjects(param, i);
+  }
+  // };
+  // allFuncDiv.append(eachFunc(param));
+}
